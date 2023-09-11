b30d60f1e0072fed340045a40bb2471ebe4fadfe
codexyan <mdcodeid@gmail.com>
2023-09-11 20:18:58 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const bodyParser = require('body-parser')
+const db = require('./connection')
+const response = require('./response')
+
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+  res.send("Express JS Connected")
+}) 
+
+app.get('/mahasiswa', (req, res) => {
+  res.send("list mahasiswa muncul")
+})
+
+app.get('/mahasiswa/:nim', (req, res) => {
+  const nim = req.params.nim
+  res.send(`spesifik mahasiswa by nim : ${nim}`)
+})
+
+app.post('/mahasiswa', (req, res) => {
+  res.send("Ini Posting")
+})
+
+app.put('/mahasiswa', (req, res) => {
+  res.send("Ini put atau update data")
+})
+
+app.delete('/mahasiswa', (req, res) => {
+  res.send("Ini delete data")
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
